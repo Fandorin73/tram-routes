92b86d33238eed87a151a3f56aeb83a88d414d85
Fandorin73 <monten49@gmail.com>
2023-06-21 22:46:49 +0300

    Created App

---
+++ src/main/java/ru/mosmetro/tramroute/TramRouteApplication.java
@@ -0,0 +1,13 @@
+package ru.mosmetro.tramroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TramRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TramRouteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mosmetro/tramroute/TramRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mosmetro.tramroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TramRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
